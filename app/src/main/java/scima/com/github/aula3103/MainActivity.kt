e0675feb10db8e3905eedd812f70f66e41ac8e35
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-07 22:30:37 -0300

    aula 07 04

--- app/src/main/java/scima/com/github/aula3103/MainActivity.kt
+++ app/src/main/java/scima/com/github/aula3103/MainActivity.kt
@@ -1,20 +1,24 @@
 package scima.com.github.aula3103
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import scima.com.github.aula3103.ui.theme.Aula3103Theme
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        // Configurando a toolbar
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.title = getText(R.string.app_title)
+
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.success))
     }
 }
